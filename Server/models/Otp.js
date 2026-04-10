340a012cc515e7f8b0dd54f4eb77348fba07422c
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-04-10 16:33:09 +0530

    Update Otp.js
    pre-middleware added for email verification

--- Server/models/Otp.js
+++ Server/models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const otpSchema = new mongoose.Schema({
     email:{
@@ -15,5 +16,23 @@ const otpSchema = new mongoose.Schema({
         expires:5*60,
     },
 });
+
+// a function -> to send mail
+async function sendVerificationEmail(email, otp){
+    try{
+
+        const mailResponse = await mailSender(email, "Verification Email From StudyNotion", otp);
+        console.log("Email sent successfully : ",mailResponse);
+
+    } catch(error){
+        console.log("Error Occured while sending Mail: ",error);
+        throw error;
+    }
+}
+
+otpSchema.pre('save',async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 
 module.exports = mongoose.model("Otp",otpSchema);
