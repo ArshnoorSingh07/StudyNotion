bcb0997598623708815ace451c6652f860cbb30b
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-04-10 15:50:05 +0530

    Update CourseProgress.js

--- Server/models/CourseProgress.js
+++ Server/models/CourseProgress.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 const courseProgress = mongoose.Schema({
     courseID:{
         type:mongoose.Schema.Types.ObjectId,
+        ref : "Course",
     },
+    completedVideos:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref : "SubSection"
+        }
+    ]
 });
 
-module.exports = mongoose.model("courseProgress",courseProgress);
+module.exports = mongoose.model("CourseProgress",courseProgress);
