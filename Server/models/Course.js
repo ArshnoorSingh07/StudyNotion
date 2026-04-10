2a5e4a9ceba8c105c2bc1bc7697bbca12a4b7d62
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-04-10 15:50:09 +0530

    Create Course.js

---
+++ Server/models/Course.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const courseSchema = mongoose.Schema({
+    courseName:{
+        type:String,
+        trim:true,
+    },
+    courseDescription:{
+        type:String,
+        trim:true,
+    },
+    instructor:{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required:true,
+    },
+    whatYouWillLearn:{
+        type:String,
+    },
+    courseContent:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Section",
+        }
+    ],
+    ratingAndReviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"RatingAndReviews",
+        }
+    ],
+    price:{
+        type:Number,
+    },
+    thumbnail:{
+        type:String,
+    },
+    tag:{
+        types:mongoose.Schema.Types.ObjectId,
+        ref:"Tag",
+    },
+    studentsEnrolled:[
+        {
+            types:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"User",
+        }
+    ],
+
+});
+
+module.exports = mongoose.model("Course",courseSchema);
