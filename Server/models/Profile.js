84af93f6dcd71ccfe066c62ae13c8f499a6041d9
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-04-14 15:57:18 +0530

    Update Profile.js

--- Server/models/Profile.js
+++ Server/models/Profile.js
@@ -10,7 +10,11 @@ const profileSchema = new mongoose.Schema({
     about:{
         type:String,
         trim:true,
-    }
+    },
+    contactNumber: {
+		type: Number,
+		trim: true,
+	},
 });
 
 module.exports = mongoose.model("Profile",profileSchema);
