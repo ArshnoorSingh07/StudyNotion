46c85894d550ffa7695006fa218b83acade49842
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-04-17 13:23:19 +0530

    Update Course.js

--- Server/controllers/Course.js
+++ Server/controllers/Course.js
@@ -148,3 +148,56 @@ exports.getAllCourses = async (req, res) => {
         });
     }
 };
+
+// get Course Details
+exports.getCourseDetails = async(req, res) => {
+    try{
+
+        // get Id
+        const {courseId} = req.body;
+
+        // find course details
+        const courseDetails = await Course.find(
+                                                {_id : courseId})
+                                                .populate(
+                                                    {
+                                                        path:"instructor",
+                                                        populate:{
+                                                            path:"additionalDetails",
+                                                        }
+                                                    }
+                                                )
+                                                .populate("category")
+                                                .populate("ratingAndreviews")
+                                                .populate({
+                                                    path:"courseContent",
+                                                    populate:{
+                                                        path:"subSection"
+                                                    },
+                                                })
+                                                .exec();
+
+        // Data Validation
+        if(!courseDetails){
+            return res.status(400).json({
+                success:false,
+                message:`Could not find the course with ${courseId}`,
+            });
+        }
+
+
+        // return response
+        return res.status(200).json({
+            success:true,
+            message:"Course details fetched successfully",
+        })
+
+    } catch (error) {
+        console.log("Error while fetching courseDetails : ",error);
+        return res.status(500).json({
+            success:false,
+            message:"Error while fetching courseDetails",
+            error:error.message,
+        })
+    }
+}
