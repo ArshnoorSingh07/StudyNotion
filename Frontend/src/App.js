ee40f2c903b063eebe818aa3c781b703454787a7
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-02 01:46:40 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,6 +9,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -69,6 +70,11 @@ function App() {
               </OpenRoute>
             }
           />
+      
+      <Route
+        path="/contact"
+        element={<Contact/>}
+      />
     
     </Routes>
 
