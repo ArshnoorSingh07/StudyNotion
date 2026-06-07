06dbabd47f8b26da9db985c1e7f227d4c9bee4bc
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-07 23:06:46 +0530

    Update RenderTotalAmount.jsx

--- Frontend/src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ Frontend/src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -8,24 +8,20 @@ const RenderTotalAmount = () => {
 
     const handleBuyCourse = () => {
         const courses = cart.map((course)=> course._id);
-        console.log("Bought these Courses: ",courses);
-        // TODO: Api Integrate -> Payment Gateway
+        //TODO -> BuyCourse(token, courses, user, navigate, dispatch)
     }
 
   return (
-    <div>
-
-        <p>Total: </p>
-        <p>Rs {total}</p>
-    
+      <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
+        <p className="mb-1 text-sm font-medium text-richblack-300">Total:</p>
+        <p className="mb-6 text-3xl font-medium text-yellow-100">₹ {total}</p>
         <IconBtn
-            text="Buy Now"
-            onclick={handleBuyCourse}
-            customClasses={"w-full justify center"}
-
+          text="Buy Now"
+          onclick={handleBuyCourse}
+          customClasses="w-full justify-center"
         />
-    </div>
-  )
+      </div>
+    )
 }
 
 export default RenderTotalAmount
