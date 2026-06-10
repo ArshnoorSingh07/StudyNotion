542ba1d18c6ccab7f7d63d008f6daf20e1727e54
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-10 10:06:10 +0530

    Update IconBtn.jsx

--- Frontend/src/Components/common/IconBtn.jsx
+++ Frontend/src/Components/common/IconBtn.jsx
@@ -17,7 +17,6 @@ const IconBtn = ({
         className={`flex items-center ${
           outline ? "border border-yellow-50 bg-transparent" : "bg-yellow-50"
         } cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 ${customClasses}`}
-        type={type}
     >
         {
             children 
