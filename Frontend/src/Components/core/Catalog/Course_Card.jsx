84ec66560b4e76ccc7273911bd6957a264df7156
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-17 18:00:19 +0530

    Update Course_Card.jsx

--- Frontend/src/Components/core/Catalog/Course_Card.jsx
+++ Frontend/src/Components/core/Catalog/Course_Card.jsx
@@ -5,37 +5,44 @@ import RatingStars from '../../common/RatingStars';
 
 const Course_Card = ({course, Height}) => {
 
-    const [averageReviewCount, setAverageReviewCount] = useState(0);
 
-    useEffect( () => {
+    const [avgReviewCount, setAvgReviewCount] = useState(0);
+
+    useEffect(()=> {
         const count = GetAvgRating(course.ratingAndReviews);
-        setAverageReviewCount(count);
+        setAvgReviewCount(count);
     },[course])
 
+
+    
   return (
-    <div>
-
-        <Link to={`/courses/${course._id}`}>
-            <div>
-                <img
-                    src={course?.thumbnail}
-                    alt='Course Thumbnail'
-                    className={`${Height} w-full rounded-xl object-cover`}
-                />
+    <>
+      <Link to={`/courses/${course._id}`}>
+        <div className="">
+          <div className="rounded-lg">
+            <img
+              src={course?.thumbnail}
+              alt="course thumnail"
+              className={`${Height} w-full rounded-xl object-cover `}
+            />
+          </div>
+          <div className="flex flex-col gap-2 px-1 py-3">
+            <p className="text-xl text-richblack-5">{course?.courseName}</p>
+            <p className="text-sm text-richblack-50">
+              {course?.instructor?.firstName} {course?.instructor?.lastName}
+            </p>
+            <div className="flex items-center gap-2">
+              <span className="text-yellow-5">{avgReviewCount || 0}</span>
+              <RatingStars Review_Count={avgReviewCount} />
+              <span className="text-richblack-400">
+                {course?.ratingAndReviews?.length} Ratings
+              </span>
             </div>
-            <div>
-                <p>{course?.courseName}</p>
-                <p>{course?.instructor?.firstName} {course?.instructor?.lastName}</p>
-                <div className='flex gap-x-3'>
-                    <span>{averageReviewCount || 0}</span>
-                    <RatingStars Review_Count={averageReviewCount}/>
-                    <span>{course?.ratingAndReviews?.length}</span>
-                </div>
-                <p>{course?.price}</p>
-            </div>
-        </Link>
-
-    </div>
+            <p className="text-xl text-richblack-5">Rs. {course?.price}</p>
+          </div>
+        </div>
+      </Link>
+    </>
   )
 }
 
