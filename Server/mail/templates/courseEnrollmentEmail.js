d50c164b6ccb3936c193ebcddfba046b41d3e85e
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-17 18:00:34 +0530

    Update courseEnrollmentEmail.js

--- Server/mail/templates/courseEnrollmentEmail.js
+++ Server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://s3-alpha.figma.com/hub/file/5087588282/bb702b0d-77ea-4ef0-b9fb-7096edbaa06f-cover.png"
                     alt="StudyNotion Logo"></a>
             <div class="message">Course Registration Confirmation</div>
             <div class="body">
@@ -79,7 +79,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                 <a class="cta" href="https://studynotion-edtech-project.vercel.app/dashboard">Go to Dashboard</a>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                    href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                    href="mailto:arshnoorsingh.05@gmail.com">info@studynotion.com</a>. We are here to help!</div>
         </div>
     </body>
     
