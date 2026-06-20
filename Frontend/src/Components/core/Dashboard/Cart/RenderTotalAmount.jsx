c773900b04aadb71ad105e6a6b4ecb6082d5e401
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-06-20 11:27:10 +0530

    Update RenderTotalAmount.jsx

--- Frontend/src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ Frontend/src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import IconBtn from '../../../common/IconBtn';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { buyCourse } from '../../../../services/operations/studentFeaturesAPI';
+import { useNavigate } from 'react-router-dom';
 
 const RenderTotalAmount = () => {
-    const {total} = useSelector( (state) => state.cart);
-    const {cart} = useSelector( (state) => state.cart);
+    const {total, cart} = useSelector( (state) => state.cart);
+    const {token} = useSelector( (state) => state.auth);
+    const {user} = useSelector( (state) => state.profile);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleBuyCourse = () => {
         const courses = cart.map((course)=> course._id);
-        //TODO -> BuyCourse(token, courses, user, navigate, dispatch)
+        buyCourse(token, courses, user, navigate, dispatch);
     }
 
   return (
