1bfb90fc292fc6ee394b425e3c7764edc6315828
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-03 12:44:25 +0530

    Update EnrolledCourses.jsx

--- Frontend/src/Components/core/Dashboard/EnrolledCourses.jsx
+++ Frontend/src/Components/core/Dashboard/EnrolledCourses.jsx
@@ -53,10 +53,15 @@ export default function EnrolledCourses() {
               <div
                 className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
                 onClick={() => {
-                  navigate(
-                    `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
-                  )
-                }}
+                console.log("Course:", course);
+                console.log("courseContent:", course.courseContent);
+                console.log("First Section:", course.courseContent?.[0]);
+                console.log("First SubSection:", course.courseContent?.[0]?.subSection?.[0]);
+
+                navigate(
+                  `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
+                );
+              }}
               >
                 <img
                   src={course.thumbnail}
