f065f47f9b2f3596241cce074436951145556213
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-03 12:44:54 +0530

    Update Catalog.jsx

--- Frontend/src/pages/Catalog.jsx
+++ Frontend/src/pages/Catalog.jsx
@@ -68,49 +68,55 @@ const Catalog = () => {
       }
 
   return (
-    <div className=" box-content bg-richblack-800 px-4" >
-      
-        <div className="mx-auto flex min-h-[260px] max-w-maxContentTab flex-col justify-center gap-4 lg:max-w-maxContent ">
-          <p className="text-sm text-richblack-300">{`Home / Catelog / `}
-            <span className="text-yellow-25">
+    <>
+      {/* Hero Section */}
+      <div className=" box-content bg-richblack-800 px-4">
+            <div className="mx-auto flex min-h-[260px] max-w-maxContentTab flex-col justify-center gap-4 lg:max-w-maxContent ">
+              <p className="text-sm text-richblack-300">
+                {`Home / Catalog / `}
+                <span className="text-yellow-25">
+                  {catalogPageData?.selectedCategory?.name}
+                </span>
+              </p>
+              <p className="text-3xl text-richblack-5">
                 {catalogPageData?.selectedCategory?.name}
-            </span>
-          </p>
-          <p className="text-3xl text-richblack-5">{catalogPageData?.selectedCategory?.name}</p>
-          <p className="max-w-[870px] text-richblack-200">{catalogPageData?.selectedCategory?.description}</p>
-        </div>
+              </p>
+              <p className="max-w-[870px] text-richblack-200">
+                {catalogPageData?.selectedCategory?.description}
+              </p>
+            </div>
+      </div>
 
           {/* Section-1 */}
-        <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
-          <div className="section_heading">
-          <div className="my-4 flex border-b border-b-richblack-600 text-sm">Courses to get you started</div>
-            <div className='flex gap-x-3'>
-                <p
-                  className={`px-4 py-2 ${
-                    active === 1
-                      ? "border-b border-b-yellow-25 text-yellow-25"
-                      : "text-richblack-50"
-                  } cursor-pointer`}
-                  onClick={() => setActive(1)}
-                >
-                  Most Popular
-                </p>
-                <p
-                  className={`px-4 py-2 ${
-                    active === 2
-                      ? "border-b border-b-yellow-25 text-yellow-25"
-                      : "text-richblack-50"
-                  } cursor-pointer`}
-                  onClick={() => setActive(2)}
-                >
-                  New
-                </p>
+          <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+            <div className="section_heading">Courses to get you started</div>
+            <div className="my-4 flex border-b border-b-richblack-600 text-sm">
+              <p
+                className={`px-4 py-2 ${
+                  active === 1
+                    ? "border-b border-b-yellow-25 text-yellow-25"
+                    : "text-richblack-50"
+                } cursor-pointer`}
+                onClick={() => setActive(1)}
+              >
+                Most Populer
+              </p>
+              <p
+                className={`px-4 py-2 ${
+                  active === 2
+                    ? "border-b border-b-yellow-25 text-yellow-25"
+                    : "text-richblack-50"
+                } cursor-pointer`}
+                onClick={() => setActive(2)}
+              >
+                New
+              </p>
             </div>
-            
             <div>
-              <CourseSlider Courses = {catalogPageData?.selectedCategory?.courses}/>
+              <CourseSlider
+                Courses={catalogPageData?.selectedCategory?.courses}
+              />
             </div>
-
           </div>
 
           {/* Section-2 */}
@@ -127,26 +133,21 @@ const Catalog = () => {
 
           {/* Section-3 */}
           <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
-            <p className="section_heading">Frequently Bought</p>
-            <div className='py-8'>
-                <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-                    {
-                      catalogPageData?.mostSellingCourses?.slice(0,4)
-                      .map((course, index) => {
-                        return(
-                          <Course_Card course = {course} key={index} Height={"h-[400px]"}/>
-                        )
-                      })
-                    }
-                </div>
+            <div className="section_heading">Frequently Bought</div>
+            <div className="py-8">
+              <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+                {catalogPageData?.mostSellingCourses
+                  ?.slice(0, 4)
+                  .map((course, i) => (
+                    <Course_Card course={course} key={i} Height={"h-[400px]"} />
+                  ))}
+              </div>
             </div>
           </div>
+          
 
-        </div>
-
-        <Footer/>
-
-    </div>
+          <Footer/> 
+    </>
   )
 }
 
