f725ab86671c46bca660c5d04954f1c0b8ef4518
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-05 20:19:41 +0530

    Update mailSender.js

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -1,33 +1,28 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer")
 
-const mailSender = async(email, title, body)=>{
-    try{
+const mailSender = async (email, title, body) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+      secure: false,
+    })
 
-        let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST ,
-            port: 465,
-            secure: true,
-            auth:{
-                user:process.env.MAIL_USER,
-                pass:process.env.MAIL_PASS,
-            }
-        })
-
-        let info = await transporter.sendMail({
-            from: 'StudyNotion - by Arshnoor Singh',
-            to: `${email}`,
-            subject: `${title}`,
-            html: `${body}`,
-        })
-
-        console.log(info);
-        return info;
-
-    } catch(error){
-        console.log("Error Occured in mailSender: ",error);
-        throw error;
-    }
+    let info = await transporter.sendMail({
+      from: `"Studynotion | CodeHelp" <${process.env.MAIL_USER}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${title}`, // Subject line
+      html: `${body}`, // html body
+    })
+    console.log(info.response)
+    return info
+  } catch (error) {
+    console.log(error.message)
+    return error.message
+  }
 }
 
-module.exports = mailSender;
+module.exports = mailSender
