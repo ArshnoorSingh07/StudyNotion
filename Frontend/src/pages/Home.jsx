912f453c646e4ef4bed6e02e821f4f346b330c9b
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-06 00:49:12 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -17,13 +17,13 @@ const Home = () => {
     <div>
 
         {/* Section 1 */}
-        <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between'>
-
+        <div className='relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-white'>
+            {/* Become a Instructor Button */}
             <Link to={"/signup"}>
 
-                <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:scale-95 w-fit'>
+                <div className='group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none'>
 
-                    <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900 shadow-lg hover:shadow-2xl'>
+                    <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900'>
 
                         <p>Become an Instructor</p>
 
@@ -35,6 +35,7 @@ const Home = () => {
 
             </Link>
 
+            {/* Heading */}
             <div className='text-center text-4xl font-semibold mt-7'>
 
                 Empower Your Future with{" "}
@@ -43,7 +44,8 @@ const Home = () => {
 
             </div>
 
-            <div className='mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+            {/* Sub Heading */}
+            <div className='-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300'>
 
                 With our online coding courses, you can learn at your own pace,
                 from anywhere in the world, and get access to a wealth of
@@ -52,7 +54,8 @@ const Home = () => {
 
             </div>
 
-            <div className='flex flex-row gap-7 mt-8'>
+            {/* CTA Buttons */}
+            <div className='mt-8 flex flex-row gap-7'>
 
                 <CTAButton active={true} linkto={'/signup'}>
                     Learn More
@@ -64,9 +67,10 @@ const Home = () => {
 
             </div>
 
-            <div className='mx-3 my-12 shadow-blue-200'>
-
+            <div className='mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200'>
+                {/* Video */}
                 <video
+                    className="shadow-[20px_20px_rgba(255,255,255)]"
                     muted
                     loop
                     autoPlay
@@ -131,7 +135,8 @@ const Home = () => {
 <a href="three/">Three</a>
 </nav>`}
 
-                    codeColor={"text-[#E2E8F0]"}
+                    codeColor={"text-yellow-25"}
+                    backgroundGradient={<div className="codeblock1 absolute"></div>}
 
                 />
 
@@ -146,15 +151,11 @@ const Home = () => {
                     position={"lg:flex-row-reverse"}
 
                     heading={
-                        <div className='text-4xl font-semibold'>
+                        <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
 
                             Start{" "}
 
-                            <HighlightText text={"coding"} />
-
-                            <br />
-
-                            <HighlightText text={"in seconds"} />
+                            <HighlightText text={"coding in seconds"} />
 
                         </div>
                     }
@@ -193,6 +194,7 @@ const Home = () => {
 </nav>`}
 
                     codeColor={"text-[#E2E8F0]"}
+                    backgroundGradient={<div className="codeblock2 absolute"></div>}
 
                 />
 
@@ -204,19 +206,19 @@ const Home = () => {
 
         {/* Section 2 */}
         <div className=' bg-pure-greys-5 text-richblack-700'>
-            <div className='homepage_bg h-[310px]'>
-                <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
-                    <div className=' h-[150px]'></div>
-
-                    <div className=' flex flex-row gap-7 text-white'>
+            <div className='homepage_bg h-[320px]'>
+                <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
+                    <div className='lg:h-[150px]'></div>
+                    {/* Explore Full Catagory Section */}
+                    <div className='flex flex-row gap-7 text-white lg:mt-8'>
                         <CTAButton active={true} linkto={'/signup'}>
-                            <div className='flex items-center gap-3'>
+                            <div className='flex items-center gap-2'>
                                 Explore Full Catalog
                                 <FaArrowRightLong/>
                             </div>
                         </CTAButton>
 
-                        <CTAButton active={false} linkto={'/signup'}>
+                        <CTAButton active={false} linkto={'/login'}>
                             <div>
                                 Learn More
                             </div>
@@ -226,14 +228,14 @@ const Home = () => {
                 </div>     
             </div>
 
-            <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
-                        
-                <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
-                    <div className='text-4xl font-semibold w-[45%]'>
+            <div className='mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 '>
+                {/* Job that is in Demand - Section 1 */}    
+                <div className='mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0'>
+                    <div className='text-4xl font-semibold lg:w-[45%] '>
                         Get the Skills you need for a
-                        <HighlightText text={"Job that is in demand"}/>
+                        <HighlightText text={"job that is in demand."}/>
                     </div>
-                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+                    <div className='flex flex-col items-start gap-10 lg:w-[40%]'>
                         <div className='text-[16px]'>
                             The modern StudyNotion is the dictates its own terms. 
                             Today, to be a competitive specialist requires more than professional skills.
@@ -244,8 +246,10 @@ const Home = () => {
                     </div>
                 </div>
 
+                {/* Timeline Section - Section 2 */}
                 <TimeLineSection />
-
+                
+                {/* Learning Language Section - Section 3 */}
                 <LearningLanguageSection />
             </div> 
 
