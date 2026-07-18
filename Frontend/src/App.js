3bf577d883697e91bbc71d26c5eb9f0bb44cdabb
Arshnoor Singh <asingh39_be23@thapar.edu>
2026-07-18 13:40:07 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
     
   const { user } = useSelector((state) => state.profile)
   return (
-   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+   <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter pt-14">
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
